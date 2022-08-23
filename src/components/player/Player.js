da9428232813fba2ad5144bc46b344a95908ff9f
Rémi Castaing <remi.castaing@gmail.com>
2022-08-23 11:14:20 +0200

    mise en forme des dés

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -12,23 +12,23 @@ function Player({ player, actif }) {
         <>
             <div className="d-flex">
                 <div className="flex-fill "></div>
-                <div className="d-flex flex-column justify-content-evenly text-center">
+                <div className="d-flex flex-column justify-content-evenly text-center" style={{ minWidth: '33%' }}>
                     <div className="p-2"><img src={logo} className={`logo filter-dice ${actif ? 'animated-logo' : ''}`} alt='logo'></img></div>
                     <div className="name">PLAYER {player.id}</div>
                     <div className="global">{player.global}</div>
                     <div className="square">
-                    <div className='round col-4 text-center p-1'>
-                        <div className={'p-3'}>Current</div>
-                        <div className='round-count p-3'>{player.round}</div>
-                        <h3>
-                            <div className="d-flex align-content-start flex-wrap">
+                        <div className='round d-flex flex-column justify-content-evenly'>
+                            <div className={'p-3'}>Current</div>
+                            <div className='round-count p-3'>{player.round}</div>
 
-                                {player.dices.map(((dice, index) => <Dice key={'d-' + player.id + '-' + index} valeur={dice} addedClass={'dices'}></Dice>))}
 
-                            </div>
-                        </h3>
+                                    <div className='row'>{player.dices.map(((dice, index) => <Dice key={'d-' + player.id + '-' + index} valeur={dice} addedClass={'dices col'}></Dice>))}</div>
+                                    
+
+
+
+                        </div>
                     </div>
-                </div>
                 </div>
                 <div className="flex-fill"></div>
             </div>
